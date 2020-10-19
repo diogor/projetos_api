fb6a322092b750baf2e43e5de7e0e70dc9c5bb1d
Diogo Lima <diogo@dr6.com.br>
2020-10-18 21:51:13 -0300

    fix team

--- projetos/models.py
+++ projetos/models.py
@@ -13,7 +13,8 @@ class Projeto(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     team = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                  related_name='membro_de')
+                                  related_name='membro_de',
+                                  blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL,
                                 related_name='projetos', on_delete=models.CASCADE)
